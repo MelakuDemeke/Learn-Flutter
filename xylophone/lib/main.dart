7e679214623783b5c0f04f5d20c6ffdcad29f60a
MelakuDemeke <melaku.demeke789@gmail.com>
2025-05-07 18:51:11 +0300

    made a sound play

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({super.key});
 
+  void playSound() {
+    final player = AudioPlayer();
+    player.play(AssetSource('note1.wav'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: TextButton(onPressed: playSound, child: Text('Play')),
         ),
       ),
     );
