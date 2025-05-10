2ee69689bf2c48c8067abfc1f64af430e36cf9b4
MelakuDemeke <melaku.demeke789@gmail.com>
2025-05-10 16:34:11 +0300

    up to 23 completed

--- destini/lib/story_brain.dart
+++ destini/lib/story_brain.dart
@@ -42,15 +42,15 @@ class StoryBrain {
   ];
 
   String getStory() {
-    return _storyData[0].storyTitle;
+    return _storyData[_storyNumber].storyTitle;
   }
 
   String getChoice1() {
-    return _storyData[0].choice1;
+    return _storyData[_storyNumber].choice1;
   }
 
   String getChoice2() {
-    return _storyData[0].choice2;
+    return _storyData[_storyNumber].choice2;
   }
 
   void nextStory(int choiceNumber) {
@@ -74,11 +74,10 @@ class StoryBrain {
   void restart() {
     _storyNumber = 0;
   }
+
+  bool buttonShouldBeVisible() {
+    return _storyNumber == 0 || _storyNumber == 1 || _storyNumber == 2;
+  }
 }
-
-//TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
-
-//TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
-
 
 //TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
