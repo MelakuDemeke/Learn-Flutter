8aa3118bef710b2d5c669e4752499bcc23f92cb9
MelakuDemeke <melaku.demeke789@gmail.com>
2025-05-12 13:03:02 +0300

    theme section

--- bmi_calculator/lib/main.dart
+++ bmi_calculator/lib/main.dart
@@ -3,26 +3,43 @@ import 'package:flutter/material.dart';
 void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
+  const BMICalculator({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: InputPage());
+    return MaterialApp(
+      home: _InputPage(),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        textTheme: TextTheme(bodyLarge: TextStyle(color: Colors.white)),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF0A0E21),
+          foregroundColor: Colors.white, // optional: sets text/icon color
+        ),
+      ),
+    );
   }
 }
 
-class InputPage extends StatefulWidget {
+class _InputPage extends StatefulWidget {
+  const _InputPage();
+
   @override
   _InputPageState createState() => _InputPageState();
 }
 
-class _InputPageState extends State<InputPage> {
+class _InputPageState extends State<_InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('BMI CALCULATOR')),
-      body: Center(child: Text('Body Text')),
+      body: DefaultTextStyle(
+        style: Theme.of(context).textTheme.bodyLarge!,
+        child: Center(child: Text('Body Text')),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          print('pressed');
+          debugPrint('pressed');
         },
         child: Icon(Icons.add),
       ),
