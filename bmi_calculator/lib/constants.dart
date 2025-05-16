149c7e1a769c5ebbf8991318551cb926ac8b073f
MelakuDemeke <melaku.demeke789@gmail.com>
2025-05-16 17:13:35 +0300

    refactor code and fix

--- bmi_calculator/lib/constants.dart
+++ bmi_calculator/lib/constants.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+const Color kActiveCardColour = Color(0xFF1D1E33);
+const Color kInactiveCardColor = Color(0xFF111328);
+const Color kButtonColor = Color(0xFFEB1555);
+
+// styles
 const kLabelTextStyle = TextStyle(fontSize: 18.0, color: Color(0xFF8D8E98));
 const kNumberTextStyle = TextStyle(fontSize: 50.0, fontWeight: FontWeight.w900);
 const kLargeButtonTextStyle = TextStyle(
@@ -7,6 +12,11 @@ const kLargeButtonTextStyle = TextStyle(
   fontWeight: FontWeight.bold,
   color: Colors.white,
 );
-const Color kActiveCardColour = Color(0xFF1D1E33);
-const Color kInactiveCardColor = Color(0xFF111328);
-const Color kButtonColor = Color(0xFFEB1555);
+
+const kTitleTextStyle = TextStyle(fontWeight: FontWeight.bold, fontSize: 50.0);
+
+const kResultTitleTextStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  color: Colors.green,
+  fontSize: 20.0,
+);
--- bmi_calculator/lib/results_page.dart
+++ bmi_calculator/lib/results_page.dart
@@ -9,37 +9,55 @@ class ResultsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('BMI CALCULATOR')),
-      body: Expanded(
-        child: ReusableCard(
-          colour: kActiveCardColour,
-          cardChild: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Center(
-                child: Text('NORMAL', style: TextStyle(color: Colors.green)),
-              ),
-              Center(
-                child: Text(
-                  '19.25',
-                  style: TextStyle(
-                    fontSize: 100.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Center(
-                
-                child: Text(
-                  'You have a normal body Good Job',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 25),
-                ),
-              ),
-            ],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Expanded(
+            child: Center(child: Text('Your Result', style: kTitleTextStyle)),
           ),
-          onPress: () {},
-        ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              colour: kActiveCardColour,
+              cardChild: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Center(child: Text('NORMAL', style: kResultTitleTextStyle)),
+                  Center(
+                    child: Text(
+                      '19.25',
+                      style: TextStyle(
+                        fontSize: 100.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      'You have a normal body Good Job',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  ),
+                ],
+              ),
+              onPress: () {},
+            ),
+          ),
+          Container(
+            color: kButtonColor,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: 80,
+            child: TextButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/result');
+              },
+              child: Text('Re-Calculate', style: kLargeButtonTextStyle),
+            ),
+          ),
+        ],
       ),
     );
   }
