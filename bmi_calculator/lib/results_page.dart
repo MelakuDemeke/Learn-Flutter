42ac9de72ba36c140ac55effda621ab190de75a6
MelakuDemeke <melaku.demeke789@gmail.com>
2025-05-16 16:43:11 +0300

    result page desinged

--- bmi_calculator/lib/results_page.dart
+++ bmi_calculator/lib/results_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'reusable_card.dart';
+import 'constants.dart';
 
 class ResultsPage extends StatelessWidget {
   const ResultsPage({super.key});
@@ -7,7 +9,38 @@ class ResultsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('BMI CALCULATOR')),
-      body: Text('Hello'),
+      body: Expanded(
+        child: ReusableCard(
+          colour: kActiveCardColour,
+          cardChild: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Center(
+                child: Text('NORMAL', style: TextStyle(color: Colors.green)),
+              ),
+              Center(
+                child: Text(
+                  '19.25',
+                  style: TextStyle(
+                    fontSize: 100.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Center(
+                
+                child: Text(
+                  'You have a normal body Good Job',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 25),
+                ),
+              ),
+            ],
+          ),
+          onPress: () {},
+        ),
+      ),
     );
   }
 }
