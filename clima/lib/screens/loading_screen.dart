19f242906150297a322906906a10aaf35297e96a
MelakuDemeke <melaku.demeke789@gmail.com>
2025-05-21 15:04:12 +0300

    Add HTTP package and implement weather data fetching in LoadingScreen

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import '../services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+const apiKey = "c0eae1ad786d869d5ca1e672d519156a";
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -8,6 +12,9 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  late double latitude;
+  late double longitude;
+
   @override
   void initState() {
     super.initState();
@@ -17,8 +24,27 @@ class _LoadingScreenState extends State<LoadingScreen> {
   Future<void> getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
+    latitude = location.latitude;
+    longitude = location.longitude;
+    getData();
+  }
+
+  Future<void> getData() async {
+    http.Response response = await http.get(
+      Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey',
+      ),
+    );
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      var temp = jsonDecode(data)['main']['temp'];
+      var condi = jsonDecode(data)['weather'][0]['id'];
+      var city = jsonDecode(data)['name'];
+      print('the temprature is $temp, and the condition is $condi in $city');
+    } else {
+      print('error occured');
+    }
   }
 
   @override
